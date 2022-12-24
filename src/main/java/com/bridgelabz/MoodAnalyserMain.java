54eb09ab6e6815eb159c776284da3408017c4081
akashp69 <akashpakhare499@gmail.com>
2022-12-24 23:29:47 +0530

    Added: Test case to return SAD by given message I am in Sad Mood

--- src/main/java/com/bridgelabz/MoodAnalyserMain.java
+++ src/main/java/com/bridgelabz/MoodAnalyserMain.java
@@ -1,30 +1,19 @@
 package com.bridgelabz;
+/**
+ * Using Class for TC : 1.1 : Given "I am in Sad Mood" message should return SAD .
+ */
 
 public class MoodAnalyserMain {
-
     private String message;
 
     public MoodAnalyserMain(String message) {
         this.message = message;
     }
-
-    /**
-     *Created a method for Happy Mood if Mood is happy then return HAPPY with using contains method
-     */
-
-    public String moodHappy() {
-        if (message.contains("happy")) {
-            return "HAPPY";
-        } else {
-            return "SAD";
-        }
-    }
     /**
      *Created a method for Sad Mood if Mood is sad then return SAD with using contains method
      */
-
     public String moodSad() {
-        if (message.contains("sad")) {
+        if (message.contains("Sad")) {
             return "SAD";
         } else {
             return "HAPPY";
