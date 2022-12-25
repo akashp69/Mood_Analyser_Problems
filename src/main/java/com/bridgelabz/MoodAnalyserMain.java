d25fdd70e33cafba2c3043eb4b536a11decf1a0c
akashp69 <akashpakhare499@gmail.com>
2022-12-25 12:59:24 +0530

    Added: Test case for Repeat the code for take I am in sad Mood  message in constructor

--- src/main/java/com/bridgelabz/MoodAnalyserMain.java
+++ src/main/java/com/bridgelabz/MoodAnalyserMain.java
@@ -1,35 +1,30 @@
 package com.bridgelabz;
 
 /**
- *Created the class for Refactor the code to take the mood message in constructor. .
+ *Created the class for Repeat the code to take the i am in sad Mood  message in constructor. .
  */
 public class MoodAnalyserMain {
     private String message;
-    public MoodAnalyserMain() {}
-    public MoodAnalyserMain(String message) {
-        this.message = message;
-    }
-
     /**
-     *Create a Method for take the mood in message
+     *Create a Method for take the mood message
      */
+        public MoodAnalyserMain(String message) {
+            this.message = message;
+        }
 
-    public String analyseMood() {
-        if (this.message.contains("sad")) {
-            return "SAD";
-        }else
-            return "HAPPY";
-    }
-
-    /**
+        public String analyseMood() {
+            if (this.message.contains("sad")) {
+                return "SAD";
+            }else
+                return "HAPPY";
+        }
+     /**
      *This is Main Method is Using for calling method and  take the mood message in constructor
-     *
      */
-
-    public static void main(String[] args) {
-        MoodAnalyserMain mood = new MoodAnalyserMain("i am sad");
-        System.out.println(mood.analyseMood());
-        MoodAnalyserMain mood2 = new MoodAnalyserMain("i am happy");
-        System.out.println(mood2.analyseMood());
+        public static void main(String[] args) {
+            MoodAnalyserMain mood = new MoodAnalyserMain("i am sad");
+            System.out.println(mood.analyseMood());
+            MoodAnalyserMain mood2 = new MoodAnalyserMain("i am happy");
+            System.out.println(mood2.analyseMood());
+        }
     }
-}
